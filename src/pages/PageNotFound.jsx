fcf9fe00d42ad5d787f799fe5ea4a989525653b2
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-05 13:48:04 +0600

    not found page added

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,38 @@
+import { Link } from "react-router-dom";
+
 export const PageNotFound = () => {
-  return <section></section>;
+  return (
+    <section>
+      <Link to={"/"}>
+        <button
+          style={{
+            padding: "10px",
+            background: "dodgerblue",
+            color: "white",
+            borderRadius: "20px",
+            marginTop: "20px",
+            marginBottom: "20px",
+            cursor: "pointer",
+          }}
+        >
+          Back to home
+        </button>
+      </Link>
+      <p>404 / Page Not Found</p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "30px",
+        }}
+      >
+        <img
+          width={"300"}
+          src="../../public/not found.svg"
+          alt="page not found img"
+        />
+      </div>
+    </section>
+  );
 };
